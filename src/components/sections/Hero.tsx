58490815e26987a7b9a66c7d64bf1150c0bbe985
Jawad Hameed Baloch <61288334+jawadhameedbaloch@users.noreply.github.com>
2026-06-06 13:41:12 +0500

    Update Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
                         className="text-[1.7rem] sm:text-4xl md:text-5xl lg:text-6xl font-bold font-display tracking-tight mb-4 md:mb-8 whitespace-nowrap overflow-visible flex justify-center w-full"
                     >
                         <span className="bg-clip-text text-transparent bg-gradient-to-r from-white via-cyber-cyan to-amethyst-purple bg-300% animate-gradient px-4 pb-2">
-                            JAWAD HAMEED BALOCH
+                        JAWAD HAMEED BALOCH
                         </span>
                     </motion.h1>
 
